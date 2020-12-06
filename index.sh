feb1cddd650764935568b981e75eccd032fe2903
Alain Armand <idkjs@users.noreply.github.com>
2020-12-06 14:28:30 +0100

    Remove quotes

--- index.sh
+++ index.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-echo "\"# Tell github that .re and .rei files are Reason, sometimes recognized as C/C++"\" >> ${PWD}/.gitattributes
+echo "# Tell github that .re and .rei files are Reason, sometimes recognized as C/C++" >> ${PWD}/.gitattributes
 echo "*.re linguist-language=Reason " >> ${PWD}/.gitattributes
 echo "*.rei linguist-language=Reason" >> ${PWD}/.gitattributes
 echo "*.res linguist-language=ReScript" >> ${PWD}/.gitattributes
