7c5005347403a358b3bb2f2777529d07befd837a
Alain Armand <aarmand.inbox@gmail.com>
2020-12-28 23:57:06 +0100

    removing quotes again

--- index.sh
+++ index.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
+message="# Tell github that .re and .rei files are Reason, sometimes recognized as C/C++"
+
 {
-echo "# Tell github that .re and .rei files are Reason, sometimes recognized as C/C++"
+echo "$message"
 echo "*.re" linguist-language=Reason
 echo "*.rei" linguist-language=Reason
 echo "*.res" linguist-language=ReScript
