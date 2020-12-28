7d67d05ab92c94af31bdf8469a7c629aab4f4939
Alain Armand <aarmand.inbox@gmail.com>
2020-12-28 23:26:13 +0100

    cleanup quotes on comment

--- index.sh
+++ index.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
-echo "# Tell github that .re and .rei files are Reason, sometimes recognized as C/C++" >> ${PWD}/.gitattributes
-echo "*.re linguist-language=Reason " >> ${PWD}/.gitattributes
-echo "*.rei linguist-language=Reason" >> ${PWD}/.gitattributes
-echo "*.res linguist-language=ReScript" >> ${PWD}/.gitattributes
-echo "*.resi linguist-language=ReScript" >> ${PWD}/.gitattributes
+{
+echo "# Tell github that .re and .rei files are Reason, sometimes recognized as C/C++"
+echo "*.re" linguist-language=Reason
+echo "*.rei" linguist-language=Reason
+echo "*.res" linguist-language=ReScript
+echo "*.resi" linguist-language=ReScript
+} >> "${PWD}/.gitattributes"
+
